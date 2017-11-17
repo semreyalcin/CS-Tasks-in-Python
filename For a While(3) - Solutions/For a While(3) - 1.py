905cdd4673d76240f81d2a8f7b8a1421819e0cf5
Selim Emre Yalçın <emre.yalcin@metu.edu.tr>
2017-11-17 20:53:57 +0200

    Revert "For a While(3) - Solutions"
    This reverts commit 468385fec1e693b664c236e01b77db7c466060fb.

--- For a While(3) - Solutions/For a While(3) - 1.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Enter an integer: "))
-summ = 0
-counter = 0
-while (x != -1):
-  summ += x
-  counter += 1
-  x = int(input("Enter an integer: "))
-
-print("Sum is : %d\tAverage is : %.2f" %(summ,(summ/counter)))
